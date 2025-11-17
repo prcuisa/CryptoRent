dfb5072e6b5bc9e804528a1f8e3d3b62b509e9ad
prcuisa <33408705+prcuisa@users.noreply.github.com>
2025-11-17 13:56:39 +0000

    main

--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+import { Download } from 'lucide-react';
 import { useState, useEffect } from 'react'
 import { Button } from '@/components/ui/button'
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
